c3e63f803580e2adc69214652b89997770742bd0
fleetain <Apkonduskar@gmail.com>
2019-09-23 16:50:58 +0530

    Third Commit

--- SimpleProject/src/simplejavaProject/JavaProject.java
+++ SimpleProject/src/simplejavaProject/JavaProject.java
@@ -3,5 +3,7 @@ package simplejavaProject;
 public class JavaProject {
     public static void main(String[] args) {
 		System.out.println("Welcome the new Programing ");
+		
+		System.out.println("Jenkin Program File");
 	}
 }
