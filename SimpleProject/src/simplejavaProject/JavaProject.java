43d1fdbf9c5616c2159f44cbce68ee547eee4dbb
fleetain <Apkonduskar@gmail.com>
2019-09-23 18:52:13 +0530

    repo Add

--- SimpleProject/src/simplejavaProject/JavaProject.java
+++ SimpleProject/src/simplejavaProject/JavaProject.java
@@ -1,9 +1,15 @@
 package simplejavaProject;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class JavaProject {
+	
+	
     public static void main(String[] args) {
+      System.setProperty("webdriver.chrome.driver","C:\\Users\\dell\\git\\SimpleProject\\SimpleProject\\chromedriver.exe");
+	    WebDriver driver=new ChromeDriver();
 		System.out.println("Welcome the new Programing ");
-		
 		System.out.println("Jenkin Program File");
 	}
 }
