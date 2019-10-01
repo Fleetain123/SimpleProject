8ceed031787768261d38e12cfe64589d4dba446f
fleetain <Apkonduskar@gmail.com>
2019-10-01 22:13:10 +0530

    some change

--- SimpleProject/src/simplejavaProject/JavaProject.java
+++ SimpleProject/src/simplejavaProject/JavaProject.java
@@ -12,16 +12,51 @@ public class JavaProject {
 	
 	
     public static void main(String[] args) throws MalformedURLException {
-     // System.setProperty("webdriver.chrome.driver","C:\\Users\\dell\\git\\SimpleProject\\SimpleProject\\chromedriver.exe");
+    	
+
+   	 WebDriver driver;
+   	String Directory = System.getProperty("user.dir");
+   	System.out.println(Directory);
+   	String str=	System.getProperty("os.name");//
+   	System.out.println(str);
+   	if(str.equals("Windows 8.1")){
+   		System.setProperty("webdriver.chrome.driver", Directory+"/chromedriver.exe");
+   		driver = new ChromeDriver();
+   		driver.manage().window().maximize();
+   		driver.get("https://www.google.com/");
+   	}
+   	else{
+   		if(str.equals("CentOS Linux")) {
+   			System.setProperty("webdriver.chrome.driver",Directory+"/chromedriver");
+   			   driver=new ChromeDriver();
+   			  driver.manage().window().maximize();
+   			driver.get("https://www.google.com/");
+   	}
+   			
+   	}
+   	}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+     /*// System.setProperty("webdriver.chrome.driver","C:\\Users\\dell\\git\\SimpleProject\\SimpleProject\\chromedriver.exe");
 	  //  WebDriver driver=new ChromeDriver();
 	    new DesiredCapabilities();
         URL serverurl = new URL("http://108.179.223.107:4444");
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         WebDriver driver = new RemoteWebDriver(serverurl,capabilities);
-    driver.get("http://www.google.com");
+    driver.get("http://www.google.com");}*/
 	    
 		
 		
 		
-	}
+	
 }
